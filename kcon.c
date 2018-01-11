41acc5cf1868fbeac1423db17086b463aa613f4b
rushyam <rushyamsonu@gmail.com>
2018-01-12 00:19:29 +0530

    linear

--- kcon.c
+++ kcon.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-int max1(int a,int b)
-{
-	return a>b?a:b;
-}
-int max(int a,int b,int c)
-{
-	return max1(max1(a,b),c);
-}
-int max_s(int a[],int l,int m,int r,int n)
-{
-	int sum=0,i;
-	int max_l = -1000001;
-	for(i=m;i>=0;i--)
-	{
-		sum += a[i%n];
-		if(sum>max_l)
-			max_l = sum;
-	}
-	sum = 0;
-	int max_r = -1000001;
-	for(i=m+1;i<=r;i++)
-	{
-		sum += a[i%n];
-		if(sum > max_r)
-			max_r = sum;
-	}
-	return (max_r+max_l);
-}
-int max_sum(int a[],int l,int r,int n)
-{
-	if(l==r)
-		return a[l%n];
-	int m = (l+r)/2;
-	return max(max_sum(a,l,m,n),max_sum(a,m+1,r,n),max_s(a,l,m,r,n));
-}
 int main()
 {
-	int f,t;
-	scanf("%d",&t);
+	long f,t;
+	scanf("%ld",&t);
 	for(f=0;f<t;f++)
 	{
-		int n,k,i,j;
-		scanf("%d%d",&n,&k);
-		int *a = (int *)malloc(n*sizeof(int));
+		long n,k,i,j;
+		scanf("%ld%ld",&n,&k);
+		long *a = (long *)malloc(n*sizeof(long));
+		long sum = 0;
 		for(i=0;i<n;i++)
-			scanf("%d",&a[i]);	
-		printf("%d\n",max_sum(a, 0, n*k-1, n));
+		{
+			scanf("%ld",&a[i]);
+			sum += a[i];
+		}
+		long m = -1000001, max_so_far = 0, max_endding_here = 0;
+		long s = sum;
+		if(k>=2)
+		{	
+			for(i=0;i<n;i++)
+			{
+				long x = sum*(k-2) + 2 * s;
+				if( x > m)
+					m = x;
+				s = s-a[i];
+			}
+		} 
+		long f = a[0];
+		for(i=0;i<n;i++)
+		{
+			if(max_endding_here + a[i] > 0)		
+				max_endding_here += a[i];
+			if(max_endding_here > max_so_far)
+				max_so_far = max_endding_here;
+			if(f < a[i])
+				f = a[i];
+		}	
+		if(max_so_far > m)
+			m = max_so_far;
+		if(f < 0)
+			m = f;
+		printf("%ld\n",m);
 	}
 	return 0;
 }
