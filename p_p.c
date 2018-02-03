d7b0a390f08bf141630a849e65eecc4a7dede21a
rushyam <rushyamsonu@gmail.com>
2018-02-04 01:55:21 +0530

    feb-18

--- p_p.c
+++ p_p.c
@@ -35,7 +35,6 @@ int quick(char s[],int a[],int l,int r)
 	if(l<r)
 	{
 		int k = quick_sort(s,a,l,r);
-	
 		quick(s,a,l,k-1);
 		quick(s,a,k+1,r);
 	}
@@ -53,13 +52,117 @@ int main()
 		for(i=0;s[i] != '\0';i++)
 			n=i;
 		n=n+1;
-		printf("%d\n", n);
 		int *a = (int *)malloc(n*sizeof(int));
 		for(i=0;i<n;i++)
 			a[i] = i+1;
 		quick(s,a,0,n-1);
+		int flag = 1;
+		if(n%2 == 0)
+		{
+			for(i=0;i<n-1;i += 2)
+			{
+				if(s[i]!=s[i+1])
+				{
+					flag = 0;
+					break;
+				}
+			}
+		}
+		// printf("%s",s);
+		// for(i=0;i<n;i++)
+		// 	printf("%d \t",a[i]);
+		if(flag==0)
+		{
+			printf("-1\n");
+			continue;
+		}
+		flag=0;
+		if(n%2)
+		{
+			for(i=0;i<n-1;)
+			{
+				if(s[i]!=s[i+1])
+				{
+					flag++;
+					i++;
+				}
+				else i+=2;
+			}
+		}
+		if(flag>1)
+		{
+			printf("-1\n");
+			continue;
+		}
+
+		if(n%2==0)
+		{
+			for(i=1;i<n/2;i+=2)
+			{
+				char temp=s[i];
+				s[i]=s[n-i];
+				s[n-i]=temp;
+				int temp2=a[i];
+				a[i]=a[n-i];
+				a[n-i]=temp2;
+			}	
+			for(i=0;i<n;i++)
+				printf("%d ",a[i]);
+			printf("\n");
+			continue;
+		}
+		i=0;
+		while(i<n)
+		{
+			if(s[i]!=s[i+1])
+				break;
+			i+=2;
+		}
+		if(i<=n/2)
+		{
+			char temp=s[i];
+			int temp2=a[i];
+			int j=i;
+			while(j<n/2)
+			{
+				s[j]=s[j+1];
+				a[j]=a[j+1];
+				j++;
+			}
+			s[j]=temp;
+			a[j]=temp2;
+		}
+		else
+		{
+			int temp2=a[i];
+			char temp=s[i];
+			int j=i;
+			while(j>n/2)
+			{
+				a[j]=a[j-1];
+				s[j]=s[j-1];
+				j--;
+			}
+			a[j]=temp2;
+			s[j]=temp;	
+		}
+
+
+		for(i=1;i<n/2;i+=2)
+			{
+				char temp=s[i];
+				s[i]=s[n-i];
+				s[n-i]=temp;
+				int temp2=a[i];
+				a[i]=a[n-i];
+				a[n-i]=temp2;
+			}
+
 		for(i=0;i<n;i++)
-			printf("%d\n",s[i] );; 
+			printf("%d ",a[i]);
+		printf("\n");
+		continue;
+
 	}
 	return 0;
 }
